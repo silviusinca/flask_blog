531de66b403bae3496cbf590a7042b066343868a
gabrielsinca <gabrielsinca14@gmail.com>
2019-09-01 22:32:03 +0300

    I worked around with forms and developed a 'join' and a 'login' page

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
+import json
+
 app = Flask(__name__)
+
+with open('strings.json') as f:
+  secret_key = json.load(f)
+
+app.config['SECRET_KEY'] = secret_key
 
 posts = [
   {
@@ -24,6 +32,27 @@ def home():
 @app.route("/about")
 def about():
   return render_template('about.html', title='About')
+
+@app.route("/join", methods=['GET', 'POST'])
+def join():
+  form = RegistrationForm()
+  if form.validate_on_submit():
+    flash(f'Account created for {form.username.data}!', 'success')
+    return redirect(url_for('home'))
+  return render_template('join.html', title='Join', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+  form = LoginForm()
+  # BAD CODE; ONLY TESTING; DELETE LATER
+  if form.validate_on_submit():
+    if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+      flash('You have been logged in!', 'success')
+      return redirect(url_for('home'))
+    else:
+      flash('Login unsuccessful. Please check username and passowrd', 'danger')
+  return render_template('login.html', title='Login', form=form)
+
 
 if __name__ == '__main__':
   app.run(debug=True)
